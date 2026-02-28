8f6cfb5b06438179c42b7afcf50a29e16c6c1428
Jokūbas Mačiulis <183698357+Yokubas@users.noreply.github.com>
2026-02-28 18:10:55 +0200

    Add code modifications

--- scripts/4_plot_real_data.py
+++ scripts/4_plot_real_data.py
@@ -1,21 +1,45 @@
 import numpy as np
 from tensorflow.keras.models import load_model
 import uproot
+import awkward as ak
 
-txt_file = "CMS_Run2016H_DoubleEG_NANOAOD_UL2016_MiniAODv2_NanoAODv9-v1_100000_file_index.txt"
+txt_file = "data/real/CMS_Run2016H_DoubleEG_NANOAOD_UL2016_MiniAODv2_NanoAODv9-v1_100000_file_index.txt"
 
 model = load_model("results/electron_classifier.h5")
 
-def create_chain_from_file_list(txt_file, max_files = -1):
+def create_chain_from_file_list(txt_file, branches, max_files=-1, max_events=None):
     files = np.loadtxt(txt_file, dtype=str)
-    
+
     if max_files != -1:
         files = files[:max_files]
 
-    # Create a "chained" view of all files
     chain = uproot.concatenate(
         [f"{f}:Events" for f in files],
-        library="np"
+        expressions=branches,      
+        entry_stop=max_events,     
+        library="ak"               
     )
 
     return chain
+
+branches = [
+    "Electron_pt",
+    "Electron_eta",
+    "Electron_miniPFRelIso_all",
+    "Electron_miniPFRelIso_chg",
+    "Electron_dz",
+    "Electron_dxy",
+    "Electron_ip3d",
+    "Jet_pt",
+    "Jet_eta",
+    "Jet_phi",
+    "Jet_btagDeepFlavB"
+]
+
+tree = create_chain_from_file_list(
+    txt_file,
+    branches,
+    max_files=1
+)
+
+print(len(tree))  # number of events
