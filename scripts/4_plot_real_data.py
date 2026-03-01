9d50080c2f1900f5879dc2f4eddffd3905ea1f36
Jokūbas Mačiulis <183698357+Yokubas@users.noreply.github.com>
2026-03-01 19:49:30 +0200

    Read real data

--- scripts/4_plot_real_data.py
+++ scripts/4_plot_real_data.py
@@ -2,25 +2,12 @@ import numpy as np
 from tensorflow.keras.models import load_model
 import uproot
 import awkward as ak
+import os 
 
 txt_file = "data/real/CMS_Run2016H_DoubleEG_NANOAOD_UL2016_MiniAODv2_NanoAODv9-v1_100000_file_index.txt"
+root_folder = "data/real/"
 
 model = load_model("results/electron_classifier.h5")
-
-def create_chain_from_file_list(txt_file, branches, max_files=-1, max_events=None):
-    files = np.loadtxt(txt_file, dtype=str)
-
-    if max_files != -1:
-        files = files[:max_files]
-
-    chain = uproot.concatenate(
-        [f"{f}:Events" for f in files],
-        expressions=branches,      
-        entry_stop=max_events,     
-        library="ak"               
-    )
-
-    return chain
 
 branches = [
     "Electron_pt",
@@ -35,6 +22,23 @@ branches = [
     "Jet_phi",
     "Jet_btagDeepFlavB"
 ]
+
+def create_chain_from_file_list(txt_file, branches, max_files=-1, max_events=None):
+    files = np.loadtxt(txt_file, dtype=str)
+
+    files = [os.path.join(root_folder, os.path.basename(f)) for f in files]
+    
+    if max_files != -1:
+        files = files[:max_files]
+
+    chain = uproot.concatenate(
+        [f"{f}:Events" for f in files],
+        expressions=branches,      
+        entry_stop=max_events,     
+        library="ak",               
+    )
+    
+    return chain
 
 tree = create_chain_from_file_list(
     txt_file,
