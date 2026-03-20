f40b6ab9aacda7b5421cfea0fedc15f03e0c038f
Jokūbas Mačiulis <183698357+Yokubas@users.noreply.github.com>
2026-03-20 16:46:19 +0200

    Read mc background and write to ROOT file

--- cpp/read_root.cpp
+++ cpp/read_root.cpp
@@ -13,8 +13,8 @@
 using namespace std;
 
 Float_t Electron_pt[100], Electron_eta[100], Electron_phi[100], Electron_mass[100];
-Float_t Electron_miniPFRelIso_all[100], Electron_miniPFRelIso_chg[100];
-Float_t Electron_dz[100], Electron_dxy[100], Electron_ip3d[100], Electron_charge[100];
+// Float_t Electron_miniPFRelIso_all[100], Electron_miniPFRelIso_chg[100];
+// Float_t Electron_dz[100], Electron_dxy[100], Electron_ip3d[100], Electron_charge[100];
 
 Float_t Jet_pt[100], Jet_eta[100], Jet_phi[100], Jet_btagDeepFlavB[100];
 
@@ -40,19 +40,18 @@ void writeRootFile(TTree* tree, const char* outFileName, double& sumGenWeight, b
     tree->SetBranchStatus("nElectron", true);
     tree->SetBranchStatus("Electron_mass", true);
 
-    if (!isMC){
-    tree->SetBranchStatus("Electron_miniPFRelIso_all", true);
-    tree->SetBranchStatus("Electron_miniPFRelIso_chg", true);
-    tree->SetBranchStatus("Electron_dz", true);
-    tree->SetBranchStatus("Electron_dxy", true);
-    tree->SetBranchStatus("Electron_ip3d", true);
+    // tree->SetBranchStatus("Electron_miniPFRelIso_all", true);
+    // tree->SetBranchStatus("Electron_miniPFRelIso_chg", true);
+    // tree->SetBranchStatus("Electron_dz", true);
+    // tree->SetBranchStatus("Electron_dxy", true);
+    // tree->SetBranchStatus("Electron_ip3d", true);
 
     tree->SetBranchStatus("nJet", true);
     tree->SetBranchStatus("Jet_pt", true);
     tree->SetBranchStatus("Jet_eta", true);
     tree->SetBranchStatus("Jet_phi", true);
     tree->SetBranchStatus("Jet_btagDeepFlavB", true);
-    }
+    
     
     Float_t genWeight;
     sumGenWeight = 0;
@@ -64,9 +63,9 @@ void writeRootFile(TTree* tree, const char* outFileName, double& sumGenWeight, b
     Float_t out_Electron_pt[2], out_Electron_eta[2], out_Electron_phi[2], out_Electron_mass[2];
     Float_t out_Jet_pt[4], out_Jet_eta[4], out_Jet_phi[4], out_Jet_btagDeepFlavB[4];
     
-    Float_t out_Electron_miniPFRelIso_all[2], out_Electron_miniPFRelIso_chg[2];
+    // Float_t out_Electron_miniPFRelIso_all[2], out_Electron_miniPFRelIso_chg[2];
     
-    Float_t out_Electron_dz[2], out_Electron_dxy[2], out_Electron_ip3d[2];
+    // Float_t out_Electron_dz[2], out_Electron_dxy[2], out_Electron_ip3d[2];
     
     if (isMC) {
         tree->SetBranchStatus("genWeight", true);
@@ -79,19 +78,18 @@ void writeRootFile(TTree* tree, const char* outFileName, double& sumGenWeight, b
     tree->SetBranchAddress("nElectron", &nElectron);
     tree->SetBranchAddress("Electron_mass", Electron_mass);
 
-    if (!isMC){
-    tree->SetBranchAddress("Electron_miniPFRelIso_all", Electron_miniPFRelIso_all);
-    tree->SetBranchAddress("Electron_miniPFRelIso_chg", Electron_miniPFRelIso_chg);
-    tree->SetBranchAddress("Electron_dz", Electron_dz);
-    tree->SetBranchAddress("Electron_dxy", Electron_dxy);
-    tree->SetBranchAddress("Electron_ip3d", Electron_ip3d);
+    // tree->SetBranchAddress("Electron_miniPFRelIso_all", Electron_miniPFRelIso_all);
+    // tree->SetBranchAddress("Electron_miniPFRelIso_chg", Electron_miniPFRelIso_chg);
+    // tree->SetBranchAddress("Electron_dz", Electron_dz);
+    // tree->SetBranchAddress("Electron_dxy", Electron_dxy);
+    // tree->SetBranchAddress("Electron_ip3d", Electron_ip3d);
 
     tree->SetBranchAddress("nJet", &nJet);
     tree->SetBranchAddress("Jet_pt", Jet_pt);
     tree->SetBranchAddress("Jet_eta", Jet_eta);
     tree->SetBranchAddress("Jet_phi", Jet_phi);
     tree->SetBranchAddress("Jet_btagDeepFlavB", Jet_btagDeepFlavB);
-    }
+    
 
     TFile *outFile = new TFile(outFileName,"RECREATE");
     TTree *outTree = new TTree("Events","Selected branches");
@@ -104,18 +102,16 @@ void writeRootFile(TTree* tree, const char* outFileName, double& sumGenWeight, b
     outTree->Branch("Electron_mass", out_Electron_mass, "Electron_mass[2]/F");
     if(isMC) outTree->Branch("genWeight", &out_genWeight, "genWeight/F");
 
-    if (!isMC) {
-    outTree->Branch("Electron_miniPFRelIso_all", out_Electron_miniPFRelIso_all, "Electron_miniPFRelIso_all[2]/F");
-    outTree->Branch("Electron_miniPFRelIso_chg", out_Electron_miniPFRelIso_chg, "Electron_miniPFRelIso_chg[2]/F");
-    outTree->Branch("Electron_dz", out_Electron_dz, "Electron_dz[2]/F");
-    outTree->Branch("Electron_dxy", out_Electron_dxy, "Electron_dxy[2]/F");
-    outTree->Branch("Electron_ip3d", out_Electron_ip3d, "Electron_ip3d[2]/F");
+    // outTree->Branch("Electron_miniPFRelIso_all", out_Electron_miniPFRelIso_all, "Electron_miniPFRelIso_all[2]/F");
+    // outTree->Branch("Electron_miniPFRelIso_chg", out_Electron_miniPFRelIso_chg, "Electron_miniPFRelIso_chg[2]/F");
+    // outTree->Branch("Electron_dz", out_Electron_dz, "Electron_dz[2]/F");
+    // outTree->Branch("Electron_dxy", out_Electron_dxy, "Electron_dxy[2]/F");
+    // outTree->Branch("Electron_ip3d", out_Electron_ip3d, "Electron_ip3d[2]/F");
 
     outTree->Branch("Jet_pt", out_Jet_pt, "Jet_pt[4]/F");
     outTree->Branch("Jet_eta", out_Jet_eta, "Jet_eta[4]/F");
     outTree->Branch("Jet_phi", out_Jet_phi, "Jet_phi[4]/F");
     outTree->Branch("Jet_btagDeepFlavB", out_Jet_btagDeepFlavB, "Jet_btagDeepFlavB[4]/F");
-    }
 
     for (int iEntry = 0; tree->LoadTree(iEntry) >= 0; ++iEntry){    
         
@@ -141,48 +137,53 @@ void writeRootFile(TTree* tree, const char* outFileName, double& sumGenWeight, b
             out_nElectron = 2;
             
             int electronIndices[2] = {idx1, idx2};
-
-            for (int i = 0; i < 2; i++) {
-
-                int id = electronIndices[i];
-
-                out_Electron_pt[i]  = Electron_pt[id];
-                out_Electron_eta[i] = Electron_eta[id];
-                out_Electron_phi[i] = Electron_phi[id];
-                out_Electron_mass[i] = Electron_mass[id];
-
-                if(!isMC) {
-                    out_Electron_miniPFRelIso_all[i] = Electron_miniPFRelIso_all[id];
-                    out_Electron_miniPFRelIso_chg[i] = Electron_miniPFRelIso_chg[id];
-
-                    out_Electron_dz[i] = Electron_dz[id];
-                    out_Electron_dxy[i] = Electron_dxy[id];
-                    out_Electron_ip3d[i] = Electron_ip3d[id];
             
-                }    
-            }
+            if (Electron_pt[idx1] > 28 && Electron_pt[idx2] > 20 && 
+                    fabs(Electron_eta[idx1]) < 2.5 && fabs(Electron_eta[idx2]) < 2.5 && 
+                    !(fabs(Electron_eta[idx1]) > 1.4442 && fabs(Electron_eta[idx1]) < 1.566) &&
+                    !(fabs(Electron_eta[idx2]) > 1.4442 && fabs(Electron_eta[idx2]) < 1.566)) {
+
+                        for (int i = 0; i < 2; i++) {
             
-            if (isMC){ 
-                out_genWeight = genWeight;
-                outTree->Fill();
-            }
+                            int id = electronIndices[i];
+            
+                            out_Electron_pt[i]  = Electron_pt[id];
+                            out_Electron_eta[i] = Electron_eta[id];
+                            out_Electron_phi[i] = Electron_phi[id];
+                            out_Electron_mass[i] = Electron_mass[id];
+            
+                            // if(!isMC) {
+                            //     out_Electron_miniPFRelIso_all[i] = Electron_miniPFRelIso_all[id];
+                            //     out_Electron_miniPFRelIso_chg[i] = Electron_miniPFRelIso_chg[id];
+            
+                            //     out_Electron_dz[i] = Electron_dz[id];
+                            //     out_Electron_dxy[i] = Electron_dxy[id];
+                            //     out_Electron_ip3d[i] = Electron_ip3d[id];
+                        
+                            // }    
+                        }
+                        
+                        if (isMC){ 
+                            out_genWeight = genWeight;
+                        }
+
+                        for (UInt_t j = 0; j < 4; j++) {
+                            if(j < nJet){
+                                out_Jet_pt[j] = Jet_pt[j];
+                                out_Jet_eta[j] = Jet_eta[j];
+                                out_Jet_phi[j] = Jet_phi[j];
+                                out_Jet_btagDeepFlavB[j] = Jet_btagDeepFlavB[j];
+                            }   else{
+                                out_Jet_pt[j] = 0;
+                                out_Jet_eta[j] = 0;
+                                out_Jet_phi[j] = 0;
+                                out_Jet_btagDeepFlavB[j] = 0;                
+                            }
+                        }
+                        outTree->Fill();
+                    }
         }
-        if (!isMC){
-            for (UInt_t j = 0; j < 4; j++) {
-                if(j < nJet){
-                    out_Jet_pt[j] = Jet_pt[j];
-                    out_Jet_eta[j] = Jet_eta[j];
-                    out_Jet_phi[j] = Jet_phi[j];
-                    out_Jet_btagDeepFlavB[j] = Jet_btagDeepFlavB[j];
-                }   else{
-                    out_Jet_pt[j] = 0;
-                    out_Jet_eta[j] = 0;
-                    out_Jet_phi[j] = 0;
-                    out_Jet_btagDeepFlavB[j] = 0;                
-                }
-            }
-            outTree->Fill();
-        }
+        
     }
 
     outFile->Write();
@@ -194,12 +195,49 @@ int main() {
     TChain* tree = createChainFromFileList("../data/real/CMS_Run2016H_DoubleEG_NANOAOD_UL2016_MiniAODv2_NanoAODv9-v1_100000_file_index.txt", 1);  
     TChain* mcTreeHigh = createChainFromFileList("../data/raw/signal/CMS_mc_RunIISummer20UL16NanoAODv9_DYJetsToLL_M-50_TuneCP5_13TeV-amcatnloFXFX-pythia8_NANOAODSIM_106X_mcRun2_asymptotic_v17-v1_30000_file_index.txt", 1);  
     TChain* mcTreeLow = createChainFromFileList("../data/raw/signal/CMS_mc_RunIISummer20UL16NanoAODv9_DYJetsToLL_M-10to50_TuneCP5_13TeV-amcatnloFXFX-pythia8_NANOAODSIM_106X_mcRun2_asymptotic_v17-v1_2520000_file_index.txt", 1);
+    // Background
+    TChain* ttbarTree = createChainFromFileList("../data/raw/background/CMS_mc_RunIISummer20UL16NanoAODv9_TTJets_TuneCP5_13TeV-amcatnloFXFX-pythia8_NANOAODSIM_106X_mcRun2_asymptotic_v17-v1_2530000_file_index.txt", 4);
+
+    TChain* twTree = createChainFromFileList("../data/raw/background/CMS_mc_RunIISummer20UL16NanoAODv9_ST_tW_top_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8_NANOAODSIM_106X_mcRun2_asymptotic_v17-v1_2520000_file_index.txt", 4);  
+    TChain* awTree = createChainFromFileList("../data/raw/background/CMS_mc_RunIISummer20UL16NanoAODv9_ST_tW_antitop_5f_NoFullyHadronicDecays_TuneCP5_13TeV-powheg-pythia8_NANOAODSIM_106X_mcRun2_asymptotic_v17-v1_270000_file_index.txt", 4);  
+    TChain* stTree = createChainFromFileList("../data/raw/background/CMS_mc_RunIISummer20UL16NanoAODv9_ST_t-channel_top_5f_InclusiveDecays_TuneCP5_13TeV-powheg-pythia8_NANOAODSIM_106X_mcRun2_asymptotic_v17-v1_120000_file_index.txt", 4);  
+    TChain* saTree = createChainFromFileList("../data/raw/background/CMS_mc_RunIISummer20UL16NanoAODv9_ST_t-channel_antitop_5f_InclusiveDecays_TuneCP5_13TeV-powheg-pythia8_NANOAODSIM_106X_mcRun2_asymptotic_v17-v1_120000_file_index.txt");  
+   
+    TChain* zzTree = createChainFromFileList("../data/raw/background/CMS_mc_RunIISummer20UL16NanoAODv9_ZZ_TuneCP5_13TeV-pythia8_NANOAODSIM_106X_mcRun2_asymptotic_v17-v1_130000_file_index.txt");  
+    TChain* wzTree = createChainFromFileList("../data/raw/background/CMS_mc_RunIISummer20UL16NanoAODv9_WZ_TuneCP5_13TeV-pythia8_NANOAODSIM_106X_mcRun2_asymptotic_v17-v1_110000_file_index.txt", 4);  
+    TChain* wwTree = createChainFromFileList("../data/raw/background/CMS_mc_RunIISummer20UL16NanoAODv9_WW_TuneCP5_13TeV-pythia8_NANOAODSIM_106X_mcRun2_asymptotic_v17-v1_130000_file_index.txt", 4);  
 
     writeRootFile(tree, "../data/processed/real/real.root", wsum, false);
+
     writeRootFile(mcTreeHigh, "../data/processed/signal/mcDYhigh.root", wsum, true);
     cout << "Wsum for mcTreeHigh: " << wsum << endl;
+
     writeRootFile(mcTreeLow, "../data/processed/signal/mcDYlow.root", wsum, true);
     cout << "Wsum for mcTreeLow: " << wsum << endl;
+
+    writeRootFile(ttbarTree, "../data/processed/background/ttbar.root", wsum, true);
+    cout << "Wsum for top-antitop (ttbar): " << wsum << endl;
+
+    writeRootFile(twTree, "../data/processed/background/tW.root", wsum, true);
+    cout << "Wsum for tW: " << wsum << endl;
+
+    writeRootFile(awTree, "../data/processed/background/antitopW.root", wsum, true);
+    cout << "Wsum for antitop W (aw): " << wsum << endl;
+
+    writeRootFile(stTree, "../data/processed/background/singletop.root", wsum, true);
+    cout << "Wsum for single top (t-channel) (st): " << wsum << endl;
+
+    writeRootFile(saTree, "../data/processed/background/sa.root", wsum, true);
+    cout << "Wsum for single antitop (t-channel) (sa): " << wsum << endl;
+
+    writeRootFile(zzTree, "../data/processed/background/zz.root", wsum, true);
+    cout << "Wsum for ZZ: " << wsum << endl;
+
+    writeRootFile(wzTree, "../data/processed/background/wz.root", wsum, true);
+    cout << "Wsum for WZ: " << wsum << endl;
+
+    writeRootFile(wwTree, "../data/processed/background/ww.root", wsum, true);
+    cout << "Wsum for WW: " << wsum << endl;
 
     return 0;
 }
